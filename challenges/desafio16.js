18d921103b5512e2aa30b4d3427194121bc210b5
VictorVidal08 <victorvidal08@hotmail.com>
2022-10-17 18:36:23 -0300

    feat: adding ultimaModificação field (using timestamp) into Big Mac meals

---
+++ challenges/desafio16.js
@@ -0,0 +1,9 @@
+db.produtos.updateMany(
+    { nome: "Big Mac" },
+    { $currentDate: {
+      lastModified: true,
+      ultimaModificacao: { $type: "timestamp" },
+    } },
+);
+
+db.produtos.find({ ultimaModificacao: { $exists: true } }, { nome: 1, _id: 0 });
